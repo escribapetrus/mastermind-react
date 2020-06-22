2aa136c932fc4a466fbcbcaf63d0c742b2bcc5f9
escribapetrus <schreiber.mmb@gmail.com>
2020-06-21 22:49:05 -0300

    Updates

---
+++ precache-manifest.0879d8baf676024bf66406ef68d53755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d22e9f031b2fe0fe68a1044edf1d4f",
+    "url": "/mastermind-react/index.html"
+  },
+  {
+    "revision": "075279a09a5f8a06162b",
+    "url": "/mastermind-react/static/css/main.29d23a20.chunk.css"
+  },
+  {
+    "revision": "34b08dea9830d8efb632",
+    "url": "/mastermind-react/static/js/2.b4bf55e7.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/mastermind-react/static/js/2.b4bf55e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075279a09a5f8a06162b",
+    "url": "/mastermind-react/static/js/main.03aa411b.chunk.js"
+  },
+  {
+    "revision": "4b101ce4a378a31068b4",
+    "url": "/mastermind-react/static/js/runtime-main.83e9b60f.js"
+  }
+]);
